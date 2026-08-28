9f71d74fc560ddfbdfd06fc59e642f783c9c1a41
Pedro Antônio <79334398+DrokaGit@users.noreply.github.com>
2026-08-28 11:06:43 -0300

    Nova versão comentada

--- ponteiros.c
+++ ponteiros.c
@@ -3,12 +3,17 @@
 
 int main()
 {
-    int a = 10;
-    int *p = &a;
+    int a;  // Nome para a posição da variavel que vai armazenar dados
+    a = 10; // Valor para a variavel acima
+    int *p; // nome á posição de memoria que vai armazenar o endereço da variavel A
+    p = &a; // Extração do endereço da variavel A
 
-
+    // o "&" nesse caso serve para pegar o endereço da variável "a"
+    // "*pa" = o CONTEUDO do PONTEIRO(endereço) da VARIAVEL A. Nesse caso, o 10.
     printf("Valor de a: %d\n", a);
     printf("Endereço de a: %p\n", (void*)&a);
+
+    *p = 20; // Mudei o valor da posição de memoria apontada para 20
     printf("Valor de p (endereço de a): %p\n", (void*)p);
     printf("Valor apontado por p: %d\n", *p);
 
